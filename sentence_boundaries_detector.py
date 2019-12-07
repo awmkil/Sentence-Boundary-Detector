c29318e58c59be4e395486ab05d1736ad620e4bc
aw.mkil <wadie.moutawakil@gmail.com>
2019-12-07 15:22:19 +0100

    The program now "works" on HTML files.

--- sentence_boundaries_detector.py
+++ sentence_boundaries_detector.py
@@ -19,7 +19,7 @@ def get_word_list_tokenized(word_list):
 class Detector:
     def __init__(self):
         self.end_of_sentence_punctuation = r"([.!?]+)$"
-        self.quotes_punctuation= '''([{'"<'''
+        self.begin_quote_punctuation = '''([{'"<'''
         self.data_set = []
         self.classifier = None
 
@@ -50,34 +50,41 @@ class Detector:
     def get_training_data(self):
         """
         Extracts the following data from the dataset:
-        - left_right_couples: Words before and after an end_of_sentence_punctuation character.
+        - left_right_couples: Words before and after an "end_of_sentence_punctuation" character.
         - labels: Label of each "left_right_couple" Can either be MOS (middle of sentece) or EOS (end of sentence).
-        - sentence_number: The sentence number
+        - sentences_index: Sentece index.
         """
 
         left_right_couples = []
         labels = []
-        sentence_number = []
+        sentences_index = []
 
         # Loop on the dataset's sentences
         for sentence_index, sentence in tqdm(enumerate(self.data_set)):
-            # Word tokenise
+
+            # Word tokenise each sentence
             sentence = get_word_list_tokenized(sentence)
+
             # Loop on each sentence's words
             for word_index, word in enumerate(sentence):
+
                 # Look for end of sentence punctuation
                 if re.search(self.end_of_sentence_punctuation, word):
-                    # Save sentence number
-                    sentence_number.append(sentence_index)
-                    # Case where end punctuation is an EOS
+
+                    # Save sentence index
+                    sentences_index.append(sentence_index)
+
+                    # Case where an end punctuation is the last character of a sentence (EOS case)
                     if word_index == len(sentence) - 1:
                         # Label EOS
                         labels.append("EOS")
-                        # Edge case when we reach the last sentence. Managed by making the right word ""
+
+                        # Edge case when we reach the last sentence.
                         if sentence_index == len(self.data_set) - 1:
                             left_right_couples.append(
                                 (sentence[word_index - 1], ""))
-                        # Save the left and right words.
+
+                        # Save the left and right word couple.
                         else:
                             left_right_couples.append(
                                 (
@@ -85,21 +92,24 @@ class Detector:
                                     self.data_set[sentence_index + 1][0],
                                 )
                             )
-                    # Case where end punctuation is an EOS
+
+                    # Case where an end punctuation is not the last character of a sentence (MOS case)
                     else:
-                        # Label is MOS otherwise
+                        # Label MOS
                         left_right_couples.append(
                             (sentence[word_index - 1],
                              sentence[word_index + 1])
                         )
                         labels.append("MOS")
 
-        return left_right_couples, labels, sentence_number
+        return left_right_couples, labels, sentences_index
 
     def get_features(self, left_right_couple):
         """
         Extracts relevant features from each left_right_couple.
+        These will feed the classifier.
         """
+
         return {
             "left_word": left_right_couple[0],
             "right_word": left_right_couple[1],
@@ -113,14 +123,15 @@ class Detector:
         """
         Creates the featureset and trains the model using NLTK's NaiveBayesClassifier.
         """
+
+        # Get training data
         left_right_couples, labels, sentence_number = self.get_training_data()
 
         # Get and format featureset
         for (left_right_couple, label) in zip(left_right_couples, labels):
-
             featuresets = [(self.get_features(left_right_couple), label)]
 
-        # Divide into training and test set, and training classifier
+        # Divide into train and test set
         train_size = 0.8
         train_limit_idx = int(len(featuresets)*train_size)
         train_set, test_set = featuresets[:train_limit_idx], featuresets[train_limit_idx:]
@@ -131,7 +142,7 @@ class Detector:
 
     def save_model(self, model_path):
         """
-                Writes the pickled representation of the model to a file
+        Writes the pickled representation of the model to a file.
         """
         print("Saving model to : " + model_path)
         with open(model_path, "wb") as f:
@@ -139,7 +150,7 @@ class Detector:
 
     def load_model(self, model_path):
         """
-                Loads the pickled representation of the model from a file
+        Loads the pickled representation of the model from a file.
         """
         print("Loading model from : " + model_path)
         with open(model_path, "rb") as f:
@@ -147,7 +158,7 @@ class Detector:
 
     def read_html(self, htlm_file_path):
         """
-        Given a `filename` string, convert the whole file into a string
+        Given a HTML file, converts it into a BeautifulSoup object.
         """
         with open(htlm_file_path, "r") as source:
             html = BeautifulSoup(source, features="html.parser")
@@ -157,85 +168,145 @@ class Detector:
         """
         Gets candidates to be labeled from a text, and their positions.
         """
+
         left_right_couples_candidates = []
         positions = []
 
+        # Word tokenize
         text = get_word_list_tokenized(nltk.word_tokenize(text))
 
+        # Loop on the words
         for word_index, word in enumerate(text):
+
             # Look for end of sentence punctuation
             if re.search(self.end_of_sentence_punctuation, word):
+
                 # Save candidate position
                 positions.append(word_index)
-                # Edge case when we reach the last sentence. Managed by making the right word ""
+
+                # Edge case when we reach the last sentence.
                 if word_index == len(text) - 1:
                     left_right_couples_candidates.append(
                         (text[word_index - 1], ""))
-                # Nominal case
+
+                # Save L/R couples
                 else:
                     left_right_couples_candidates.append(
                         (text[word_index - 1], text[word_index + 1]))
 
         return left_right_couples_candidates, positions
 
-    def label_obvious_candidates(self, left_right_couples_candidates):
+    def label_HTML(self, htlm_file_path):
         """
-        This function labels the couples where the right word is "<" as EOS
-        since when a balise is present after a sentence, it means that the
-        paragraph is over.
+        Given an HTML file, wrap each sentence in a <span> tag and returns the modified HTML file.
         """
 
-    def label_candidates_for_text(self, htlm_file_path):
-        html = self.read_html(htlm_file_path)
-        text = html.get_text('\n')
+        # Read HTML
+        html_soup = self.read_html(htlm_file_path)
 
-        left_right_couples_candidates, positions = self.get_candidates(text)
-
+        # Init prediction list
         predictions = []
-        span_tag = html.new_tag('span')
 
-        # Get the features and classify
-        for index, left_right_couples_candidate in enumerate(left_right_couples_candidates):
-            predictions.append(self.classifier.classify(
-                self.get_features(left_right_couples_candidate)))
+        # Loop on the HTML text portions
+        for text in html_soup.find_all(text=True):
 
-        input_text_words = get_word_list_tokenized(nltk.word_tokenize(text))
-        output_text = ""
+                # Copy text portion to avoid dynamic typing problems
+            str_text = str(text)
 
-        for index, position in enumerate(positions):
-            if index == 0:
-            	for i in input_text_words[0: position + 1]:
-                	# Ne pas insérer d'éspaces avant une ponctuation ou avant le mot suivant un [(<
+            # For each text portion get candidates and their positions
+            left_right_couples_candidates, positions = self.get_candidates(
+                str_text)
+
+            # Extract features from candidates and predict labels using the classifier
+            for index, left_right_couples_candidate in enumerate(left_right_couples_candidates):
+                predictions.append(self.classifier.classify(
+                    self.get_features(left_right_couples_candidate)))
+
+            # Word tokenize the text portion
+            input_text_words = get_word_list_tokenized(
+                nltk.word_tokenize(str_text))
+
+            # String with text + wrapper
+            output_text = ""
+
+            # Edge case for empty text portions / portions with no candidates (no EOS punctuation)
+            if positions == [] or len(str_text) < 2:
+
+                # Loop on text words
+                for i in input_text_words:
+
+                    # Copy with no space before if the word is a punctuation word
                     if i in string.punctuation:
                         output_text += "".join([i])
+
+                    # Else, copy with a space before
                     else:
                         output_text += "".join([" " + i])
 
+            # Nominal case, the text portion contains candidates
             else:
-                for i, j in zip(input_text_words[positions[index - 1] + 1: position + 1], input_text_words[positions[index - 1]: position + 1]):
-                	# Ne pas insérer d'éspaces avant une ponctuation ou avant le mot suivant un [(<
-                    if i in string.punctuation or j in self.quotes_punctuation:
-                        output_text += "".join([i])
+                # Open wrapper in the begining of the text portion
+                output_text += "<span>"
+
+                # Loop on candidates positions
+                for index, position in enumerate(positions):
+
+                        # Force EOS label on the last EOS punctuation of the text
+                    if index == len(positions)-1:
+                        predictions[index] == "EOS"
+
+                    # Edge case for index 0
+                    if index == 0:
+                        for i in input_text_words[0: position + 1]:
+                            # Copy with no space before if the word is a punctuation word.
+                            if i in string.punctuation:
+                                output_text += "".join([i])
+                            # Else, copy with a space before
+                            else:
+                                output_text += "".join([" " + i])
+
+                    # Nominal case
                     else:
-                        output_text += "".join([" " + i])
+                        for i, j in zip(input_text_words[positions[index - 1] + 1: position + 1], input_text_words[positions[index - 1]: position + 1]):
+                            # Copy with no space before if the current word is a punctuation word or the begining of a quote.
+                            if i in string.punctuation or j in self.begin_quote_punctuation:
+                                output_text += "".join([i])
+                            # Else, copy with a space before
+                            else:
+                                output_text += "".join([" " + i])
 
-            if predictions[index] == "EOS":
-                output_text += "<EOS>\n\n"
+                    # Close span tag when an end of sentence is detected and open a new one.
+                    if predictions[index] == "EOS":
+                        output_text += "</span> <span>"
 
-            else:
-                output_text += "<MOS>"
+                    # Signal when a MOS punctuation character is detected.
+                    else:
+                        output_text += "[MOS]"
 
-        return output_text
+                # Delete last <span/> at the end of paragraph.
+                output_text = output_text[:-6]
+
+            # Replace text in BeautifulSoup object.
+            text.replace_with(output_text)
+
+        # Pretiffy and save to new object.
+        new_html_data = html_soup.prettify()
+
+        return new_html_data
+
+    def save_HTML(self, new_html_data, htlm_file_path):
+        """
+        Writes BeautifulSoup to HTML file.
+        """
+        with open(htlm_file_path, 'w') as f:
+            f.write(new_html_data)
 
 
 detector = Detector()
+
 # detector.train_model()
 # detector.save_model("./model.pickle")
+
 detector.load_model("./model.pickle")
-
-output = detector.label_candidates_for_text("./test_book.html")
-
-with open("output.txt", "w") as f:
-    f.write(output)
-
-# print(left_right_couples_candidates)
+new_html_data = detector.label_HTML("./sample_files/the_little_prince.html")
+detector.save_HTML(new_html_data, "./fini.html")
